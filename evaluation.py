7a7a3bedb23c7862b269e44b5d14ae3fde52c173
EltonLCC <lccechai@gmail.com>
2023-07-17 23:00:46 +0800

    update result

--- evaluation.py
+++ evaluation.py
@@ -1,12 +1,22 @@
 import json
 from pathlib import Path
+import interval
 
-UR_FALL_DIR = "dataset/UR Fall Detection Dataset"
-MULTICAM_DIR = "dataset/Multicam"
-LE2I_DIR = "dataset/Le2i Fall Detection Dataset"
-FALL_DETECTION_DIR = "dataset/Fall Detection Dataset"
+UR_FALL_INFERENCE = "inference_result/UR_FALL.json"
+MULTI_CAM_INFERENCE = "inference_result/multi_cam.json"
+LE2I_INFERENCE = "inference_result/le2i.json"
 
-MULTI_CAM_GROUND = "/Users/eltonli/Desktop/dataset/Multicam/ground_truth.json"
+MULTI_CAM_GROUND = "dataset/Multicam/ground_truth.json"
+LE2I_HOME_GROUND = "dataset/Le2i_Fall_Detection_Dataset/home/Annotation_files"
+LE2I_COFFEE_GROUND = "dataset/Le2i_Fall_Detection_Dataset/Coffee_room/Annotation_files"
+
+def read_first_two_line(file_path):
+    file1 = open(file_path, 'r')
+    Lines = file1.readlines()
+    if Lines[0] == '0\n' and Lines[1] == '0\n':
+        return 0
+    else:
+        return 1
 
 
 class ModelResult:
@@ -39,8 +49,10 @@ class ModelResult:
         pass
 
     def get_dict(self):
-        precision = len(self.true_pos) / (len(self.true_pos) + len(self.false_pos))
-        recall = len(self.true_pos) / (len(self.true_pos) + len(self.false_neg))
+        precision = len(self.true_pos) / \
+            (len(self.true_pos) + len(self.false_pos))
+        recall = len(self.true_pos) / \
+            (len(self.true_pos) + len(self.false_neg))
         f1_score = 2 * precision * recall / (precision + recall)
 
         return_result = {
@@ -66,55 +78,166 @@ class ModelResult:
         return return_result
 
 
-def video_fall():
-    pass
-
-
 def le2i():
-    pass
+    result = ModelResult()
 
-
-def multicam():
-    file = open(MULTI_CAM_GROUND)
-    data = json.load(file)
+    file = open(LE2I_INFERENCE)
+    inference_result = json.load(file)
     file.close()
-    ground_truth = {}
-    for vid in data:
-        temp = [i for i in range(data[vid][0]["start"] + 1)]
-        for interval in data[vid]:
-            temp += [i for i in range(interval["start"], interval["end"] + 1)]
-        print(temp)
-        break
-    pass
+
+    count = 0
+    for video in inference_result['coffee_room']:
+        for id in video:
+            file_path = f'{LE2I_COFFEE_GROUND}/{id}'
+            file_path = file_path.replace('.avi', '.txt')
+            if read_first_two_line(file_path) == 1:
+                if 1 in video[id]:
+                    # Fall in the video and the model output as yes
+                    result.add_tp(id)
+                else:
+                    # Fall in the video and the model output as no
+                    result.add_fn(id)
+            else:
+                if 1 in video[id]:
+                    # Fall not in the video and the model output as yes
+                    result.add_fp(id)
+                else:
+                    # Fall not in the video and the model output as no
+                    result.add_tn(id)
+
+    for video in inference_result['home']:
+        for id in video:
+            file_path = f'{LE2I_HOME_GROUND}/{id}'
+            file_path = file_path.replace('.avi', '.txt')
+            if read_first_two_line(file_path) == 1:
+                if 1 in video[id]:
+                    # Fall in the video and the model output as yes
+                    result.add_tp(id)
+                else:
+                    # Fall in the video and the model output as no
+                    result.add_fn(id)
+            else:
+                if 1 in video[id]:
+                    # Fall not in the video and the model output as yes
+                    result.add_fp(id)
+                else:
+                    # Fall not in the video and the model output as no
+                    result.add_tn(id)
+    stat_data = result.get_dict()
+    print(stat_data["score"])
+    with open("LE2I_EVAL.json", "w") as outfile:
+        json.dump(stat_data, outfile)
+
+    stat_data = result.get_dict()
+    print(stat_data["score"])
+
+
+def get_sequence(ipt_list: list) -> list:
+    result = ModelResult()
+
+    file = open(UR_FALL_INFERENCE)
+    inference_result = json.load(file)
+    file.close()
+
+    for video in inference_result['fall']:
+        for id in video:
+            if 1 in video[id]:
+                # Fall in the video and the model output as yes
+                result.add_tp(id)
+            else:
+                # Fall in the video and the model output as no
+                result.add_fn(id)
+
+    for video in inference_result['adl']:
+        for id in video:
+            if 1 in video[id]:
+                # Fall not in the video and the model output as yes
+                result.add_fp(id)
+            else:
+                # Fall not in the video and the model output as no
+                result.add_tn(id)
+
+# def multicam():
+#     '''
+#     1: Walking, standing up
+#     2: Falling
+#     3: Lying on the ground
+#     4: Crounching
+#     5: Moving down
+#     6: Moving up
+#     7: Sitting
+#     8: Lying on a sofa
+#     9: Moving horizontaly
+#     '''
+#     result = ModelResult()
+
+#     file = open(MULTI_CAM_INFERENCE)
+#     inference_result = json.load(file)
+#     file.close()
+
+#     file = open(MULTI_CAM_GROUND)
+#     ground_truth = json.load(file)
+#     file.close()
+
+#     for chute in inference_result:
+#         print(chute)
+#         for cam in inference_result[chute]:
+#             print(f'{chute} {cam}')
+#             fall_interval = get_sequence(inference_result[chute][cam])
+#             # Find out all falling interval
+#             drop_interval = []
+#             for section in fall_interval:
+#                 # print(interval.interval[section])
+#                 for fall in ground_truth[chute]:
+#                     if fall['code'] != 2:
+#                         continue
+#                     # print(interval.interval[fall['start'], fall['end']])
+#                     if (interval.interval[fall['start'], fall['end']] & interval.interval[section]):
+#                         result.add_tp(id)
+#                         print(interval.interval[fall['start'], fall['end']], interval.interval[section])
+#                         print('TRUE POSITIVE')
+#                         drop_interval.append(section)
+#                         return
+
+
+
 
 
 def ur_fall():
     result = ModelResult()
 
-    fall_video_dir = UR_FALL_DIR / Path("fall")
-    for fall_video in fall_video_dir.glob("*"):
-        # Call fall detection
-        det_result = [0, 0]
-        if 1 in det_result:
-            result.add_tp(fall_video)
-        else:
-            result.add_fn(fall_video)
+    file = open(UR_FALL_INFERENCE)
+    inference_result = json.load(file)
+    file.close()
 
-    daily_video_dir = UR_FALL_DIR / Path("adl")
-    for fall_video in daily_video_dir.glob("*"):
-        # Call fall detection
-        det_result = [0, 0]
-        if 1 in det_result:
-            result.add_fp(fall_video)
-        else:
-            result.add_tn(fall_video)
+    for video in inference_result['fall']:
+        for id in video:
+            if 1 in video[id]:
+                # Fall in the video and the model output as yes
+                result.add_tp(id)
+            else:
+                # Fall in the video and the model output as no
+                result.add_fn(id)
+
+    for video in inference_result['adl']:
+        for id in video:
+            if 1 in video[id]:
+                # Fall not in the video and the model output as yes
+                result.add_fp(id)
+            else:
+                # Fall not in the video and the model output as no
+                result.add_tn(id)
+
 
     stat_data = result.get_dict()
-    print(stat_data['score'])
+    print(stat_data["score"])
+    with open("UR_FALL_EVAL.json", "w") as outfile:
+        json.dump(stat_data, outfile)
 
 
 def main():
     ur_fall()
+    le2i()
 
 
 if __name__ == "__main__":
